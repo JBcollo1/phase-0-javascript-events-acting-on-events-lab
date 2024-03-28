91eeacbc7e4e1ba1cdcb1b13266e75268a98af96
jbcollins254@gmail.com <jbcollins254@gmail.com>
2024-03-28 15:04:57 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+let dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#FF69B4"
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+function moveDodgerRight(){
+    const rightNumber = dodger.style.left.replace("px","");
+    const right = parseFloat(rightNumber, 10);
+
+    if(right > 0) {
+        dodger.style.left = `${right + 1}px`
+    }
+}
